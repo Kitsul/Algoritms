4a6dec56d4fa8a14ce69fa8c2ba49005ccbf266b
Yuriy <yuri.kitsul@hys-enterprise.com>
2018-08-09 22:08:08 +0300

    added LinkedListNode

---
+++ Algoritms/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkedList;
+namespace Algoritms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListNode<int> first = new LinkedListNode<int>(5);
+            LinkedListNode<int> second = new LinkedListNode<int>(10);
+            first.Next = second;
+            Console.WriteLine(first.Next.Value);
+        }
+    }
+}
